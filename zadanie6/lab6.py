7700a0f95db678dd5b92343d73faa4c2f6ccbabc
Maro94 <40964663+Maro94@users.noreply.github.com>
2021-01-11 00:17:23 +0100

    Update lab6.py

--- zadanie6/lab6.py
+++ zadanie6/lab6.py
@@ -4,3 +4,12 @@ with open('face.png', 'rb') as byte_reader:
     print(byte_reader.read(2))
     print(byte_reader.read(1))
     print(byte_reader.read(1))
+
+    
+#Wynik   
+'\x89'
+b'PNG'
+b'\r\n'
+b'\x1a'
+b'\n'
+PS C:\U
